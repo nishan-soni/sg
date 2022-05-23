c2497b733ddc8a09969b62759a97de527a320699
Nishan Soni <nishan.soni2@gmail.com>
2022-05-23 12:08:42 -0600

    changes to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Application from "./pages/Application/Application";
-import Home from './pages/Home/Home';
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element = {<Application/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Application />
     </div>
   );
 }
